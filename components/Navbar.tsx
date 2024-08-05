bbf4deb18204f33c49005f1e6eb494bab6b3ce4c
Nick Rabone <n.rabone72@aol.com>
2024-08-06 00:45:34 +0100

    Navbar completed for now.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,17 +2,44 @@ import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../images/logo.png'
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
 const Navbar = () => {
   return ( 
-    <div className='bg-[#111] dark:bg-[#87ceeb] py-2 px-5 flex justify-between text-[#ff4500]'>
+    <div className='bg-[#111] dark:bg-[#ccc] py-2 px-5 flex justify-between text-[#111]'>
       <Link href='/'>
-        <Image src={logo} alt='NimiraTech Dashboard' width={40} />
-        
+        <Image src={logo} alt='NimiraTech Dashboard' width={40} /> 
       </Link>
-      <Avatar>
-        <AvatarFallback className='bg-[#87ceeb]'>NR</AvatarFallback>
-      </Avatar>
+      <DropdownMenu>
+        <DropdownMenuTrigger className='focus:outline-none'>
+          <Avatar>
+            <AvatarFallback className='bg-[#ccc]'>NR</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href='/profile'>
+              Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>Projects</DropdownMenuItem>
+          <DropdownMenuItem>Research</DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='auth'>
+              Logout
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>  
+      </DropdownMenu>
     </div>
    )
 }
