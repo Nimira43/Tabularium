f2bd209d2c020a6638c100aa3be2e9d9851871a3
Nick Rabone <n.rabone72@aol.com>
2024-08-06 00:16:49 +0100

    Created fallback avatar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../images/logo.png'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 
 const Navbar = () => {
   return ( 
-    <div className='bg-[#111] dark:bg-[#87ceeb] py-2 px-5 flex justify-between text-[#ff4500]'>Navbar</div>
+    <div className='bg-[#111] dark:bg-[#87ceeb] py-2 px-5 flex justify-between text-[#ff4500]'>
+      <Link href='/'>
+        <Image src={logo} alt='NimiraTech Dashboard' width={40} />
+        
+      </Link>
+      <Avatar>
+        <AvatarFallback className='bg-[#87ceeb]'>NR</AvatarFallback>
+      </Avatar>
+    </div>
    )
 }
  
