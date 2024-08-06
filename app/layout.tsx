b7e02aa2a4cfea411a3cdd77dec5b6a3e0d8bdde
Nick Rabone <n.rabone72@aol.com>
2024-08-06 01:51:26 +0100

    Set up default sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
       <body className={poppins.className}>
         <Navbar />
         <div className="flex">
-          <div>
+          <div className='hidden md:block h-[100vh]'>
             <Sidebar />
           </div>
-          <div>
+          <div className='p-5 w-full md:max-w-[1140px]'>
             {children}
           </div>
         </div>
