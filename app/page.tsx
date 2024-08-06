814d6cbc05db16f2104040c06079984137c3132e
Nick Rabone <n.rabone72@aol.com>
2024-08-06 03:43:17 +0100

    Set up DashboardCard.tsx as a card component for the overall main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import { Button } from "@/components/ui/button"
+import DashboardCard from "@/components/dashboard/DashboardCard"
+import { Ticket } from 'lucide-react'
 
 export default function Home() {
   return (
-    <h1>NimiraTech Dashboard</h1>
+    <>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title='Tickets'
+          count={27}
+          icon={<Ticket className='text-[#111]' size={72} />}
+         />
+      </div>
+    </>
   )
 }
