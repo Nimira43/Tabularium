f9a18da4c7df84697b5787d11778b1f7863fad57
Nick Rabone <n.rabone72@aol.com>
2024-08-06 02:43:45 +0100

    Sidebar completed

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -16,7 +16,9 @@ import {
   FolderCode,
   Terminal,
   Settings,
-  UserRound
+  UserRound,
+  Mail,
+  Phone,
 } from 'lucide-react'
 
 import Link from 'next/link'
@@ -28,15 +30,45 @@ const Sidebar = () => {
       <CommandList>
         <CommandEmpty>No results founds</CommandEmpty>
         <CommandGroup heading='Suggestions'>
-          <CommandItem>Calendar</CommandItem>
-          <CommandItem>Search</CommandItem>
-          <CommandItem>Calculator</CommandItem>
+          <CommandItem>
+            <CircleGauge className='mr-2 h-4 w-4' />
+            <Link href='/'>Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <NotebookPen className='mr-2 h-4 w-4' />
+            <Link href='/posts'>Documentation</Link>
+          </CommandItem>
+          <CommandItem>
+            <FolderCode className='mr-2 h-4 w-4' />
+            <Link href='#'>Code Editor</Link>
+          </CommandItem>
+          <CommandItem>
+            <Terminal className='mr-2 h-4 w-4' />
+            <Link href='/'>Terminal</Link>
+          </CommandItem>
         </CommandGroup>
         <CommandSeparator />
-        <CommandGroup heading='Settings'>
-          <CommandItem>Profile</CommandItem>
-          <CommandItem>Billing</CommandItem>
-          <CommandItem>Settings</CommandItem>
+        <CommandGroup heading='Quick Access'>
+          <CommandItem>
+            <UserRound className='mr-2 h-4 w-4' />
+            <span>Profile</span>
+            <CommandShortcut>^u</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Mail className='mr-2 h-4 w-4' />
+            <span>Emails</span>
+            <CommandShortcut>^e</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Phone className='mr-2 h-4 w-4' />
+            <span>Contacts</span>
+            <CommandShortcut>^c</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className='mr-2 h-4 w-4' />
+            <span>Settings</span>
+            <CommandShortcut>^s</CommandShortcut>
+          </CommandItem>
         </CommandGroup>
       </CommandList>
     </Command>
