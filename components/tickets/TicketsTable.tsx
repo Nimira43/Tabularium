0c7ea7e936e63a1030e133ac3c374a2189d9a714
Nimira43 <n.rabone72@aol.com>
2025-03-17 16:57:01 +0000

    Starting to set up the table in TicketsTable

--- components/tickets/TicketsTable.tsx
+++ components/tickets/TicketsTable.tsx
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '@/components/ui/table'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, TableCaption} from '@/components/ui/table'
 import Link from 'next/link'
 import tickets from '@/data/tickets'
 import { Ticket } from '@/types/tickets'
@@ -8,13 +8,15 @@ interface TicketsTablesProps {
   title?: string
 }
 
-
 const TicketsTable = ({limit, title}: TicketsTablesProps) => {
   return (  
     <div className='mt-10'>
       <h3 className='text-2xl mb-4 font-semibold'>
         {title ? title : 'Tickets' }
       </h3>
+      <Table>
+        <TableCaption>Recent Tickets </TableCaption>
+      </Table>
     </div>
   )
 }
