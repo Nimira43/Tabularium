7d7c00a3772ca4a1e2bd92a70c55462249359376
Nimira43 <n.rabone72@aol.com>
2025-05-15 21:12:49 +0100

    Adding height and width to the icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,15 @@ import { VscDashboard } from 'react-icons/vsc'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 
-
 const Navbar = () => {
   return ( 
     <div className='bg-grey-light-extra dark:bg-dark text-dark dark:text-light py-2 px-5 flex justify-between'>
       <Link href='/'>
-        <VscDashboard className='text-main text-4xl mt-1'/>
+        <VscDashboard className='text-main h-10 w-10 mt-1'/>
       </Link>
       
       <DropdownMenu>
-        <DropdownMenuTrigger>
+        <DropdownMenuTrigger className='focus:outline-none'>
           <Avatar>
             <AvatarImage src='#' alt='#' />
             <AvatarFallback className='bg-main text-light'>NR</AvatarFallback>
