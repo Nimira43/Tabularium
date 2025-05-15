90d49c70ba02d4a024b8965bd7b87643db8df92d
Nimira43 <n.rabone72@aol.com>
2025-05-15 18:02:26 +0100

    Reboot project. Created new Next JS 14 boilerplate. Name of project changed to Tabularium

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,95 @@
-import DashboardCard from '@/components/dashboard/DashboardCard'
-import TicketsTable from '@/components/tickets/TicketsTable'
-import { Ticket, Folder, UserRound, TriangleAlert } from 'lucide-react'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
-        <DashboardCard
-          title='Tickets'
-          count={27}
-          icon={<Ticket className='text-[#111]' size={72} />}
-         />
-        <DashboardCard
-          title='Categories'
-          count={15}
-          icon={<Folder className='text-[#111]' size={72} />}
-         />
-        <DashboardCard
-          title='Users'
-          count={76}
-          icon={<UserRound className='text-[#111]' size={72} />}
-         />
-        <DashboardCard
-          title='Alerts'
-          count={38}
-          icon={<TriangleAlert className='text-[#111]' size={72} />}
-         />
-      </div>
-      <TicketsTable title='Latest Tickets'/>
-    </>
-  )
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
 }
