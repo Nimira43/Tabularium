b743af7575ae2ea3e86e953fcdf7948cf6ed3d21
Nimira43 <n.rabone72@aol.com>
2025-05-15 21:03:20 +0100

    Setting up AvatarFallback

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
 import { VscDashboard } from 'react-icons/vsc'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+
 
 const Navbar = () => {
   return ( 
     <div className='bg-grey-light-extra dark:bg-dark text-dark dark:text-light py-2 px-5 flex justify-between'>
       <Link href='/'>
-        <VscDashboard className='text-main text-3xl'/>
+        <VscDashboard className='text-main text-4xl mt-1'/>
+        
       </Link>
+      <Avatar>
+        <AvatarImage src='#' alt='#' />
+        <AvatarFallback className='bg-main text-light'>NR</AvatarFallback>
+      </Avatar>
     </div>
   )
 }
