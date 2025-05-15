5b9a4e3cf7f3e0c863b16bbf70bd3b19f8effdf1
Nimira43 <n.rabone72@aol.com>
2025-05-15 20:45:36 +0100

    Added icon for logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+import { VscDashboard } from 'react-icons/vsc'
+
 const Navbar = () => {
   return ( 
     <div className='bg-grey-light-extra dark:bg-dark text-dark dark:text-light py-2 px-5 flex justify-between'>
-      Navbar
+      <Link href='/'>
+        <VscDashboard className='text-main text-3xl'/>
+      </Link>
     </div>
   )
 }
