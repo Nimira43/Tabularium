616c28dce01bffe91f55f06fae6e3f0bca575be4
Nimira43 <n.rabone72@aol.com>
2025-05-15 18:13:57 +0100

    Created theme colour in tailwind.config and updated content.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#ff4500',
+        'support': '#008080',
+        'light': '#fffcfa',
+        'dark': '#111',
+        'grey-dark': '#333',
+        'grey-medium': '#898989',
+        'grey-light': '#ccc',
+        'grey-light-dark': '#eee',
+      }
+    },
   },
   plugins: [],
 }
