6122b624f4012b0cbbee98cbdbecd6fa876fea20
Nimira43 <n.rabone72@aol.com>
2025-05-15 21:31:20 +0100

    Setting up Comand components and importing Link

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,27 @@
+import { CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandSeparator } from 'cmdk'
+import { Command } from 'lucide-react'
+import Link from 'next/link'
+
 const Sidebar = () => {
   return ( 
-    <div>
-      Sidebar
-    </div>
+    <Command>
+      <CommandInput placeholder='Type a command or search...' />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading='Suggestions'>
+          <CommandItem>Calendar</CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading='Settings'>
+          <CommandItem>Profile</CommandItem>
+          <CommandItem>Billing</CommandItem>
+          <CommandItem>Settings</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+
    )
 }
  
