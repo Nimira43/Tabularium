e97bbf2649dd879ee128b668462ea1fb2d2257f3
Nimira43 <n.rabone72@aol.com>
2025-05-25 03:49:21 +0100

    Completed fire dashboard card.

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -3,8 +3,14 @@ import { Card, CardContent } from '../ui/card'
 
 const DashboardCard = () => {
   return ( 
-    <Card className='bg-grey-light-extra dark:bg-dark text-dark dark:text-light p-4 pb-0'>
-      
+    <Card className='bg-grey-light-extra dark:bg-dark  p-4 pb-0'>
+      <CardContent>
+        <h3 className='text-3xl text-center mb-4 font-medium text-dark dark:text-light'>Posts</h3>
+        <div className='flex gap-5 justify-center items-center'>
+          <BsNewspaper className='text-main' size={72} />
+          <h3 className='text-5xl font-normal'>43</h3>
+        </div>
+      </CardContent>
 
     </Card>
    )
