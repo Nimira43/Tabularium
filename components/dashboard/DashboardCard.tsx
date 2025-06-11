981887740a9d92db3520bc17284c061a1ad9c470
Nimira43 <n.rabone72@aol.com>
2025-06-11 16:07:40 +0100

    Created DashboardCardProps interface.

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,5 +1,12 @@
 import { BsNewspaper } from 'react-icons/bs'
 import { Card, CardContent } from '../ui/card'
+import { LucideIcon } from 'lucide-react'
+
+interface DashboardCardProps {
+  title: string
+  count: number
+  icon:  React.ReactElement<LucideIcon>
+}
 
 const DashboardCard = () => {
   return ( 
@@ -8,7 +15,7 @@ const DashboardCard = () => {
         <h3 className='text-3xl text-center mb-4 font-medium text-dark dark:text-light'>Posts</h3>
         <div className='flex gap-5 justify-center items-center'>
           <BsNewspaper className='text-main' size={72} />
-          <h3 className='text-5xl font-normal'>43</h3>
+          <h3 className='text-5xl font-normal text-dark dark:text-light'>43</h3>
         </div>
       </CardContent>
 
