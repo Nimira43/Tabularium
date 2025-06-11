8a5e14babac440ebf5913c1f0b4554c53a6ecc02
Nimira43 <n.rabone72@aol.com>
2025-06-11 16:25:01 +0100

    Changing all icons to Lucide Icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,5 @@
 import { Command, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandSeparator, CommandShortcut } from '@/components/ui/command'
-import { RxDashboard } from 'react-icons/rx'
-import { BsNewspaper } from 'react-icons/bs'
-import { PiFolders, PiUserCircle  } from 'react-icons/pi'
-import { ImCreditCard } from 'react-icons/im'
-import { TbSettings } from 'react-icons/tb'
+import { LayoutDashboard, Milestone, FolderClosed, UserCircle, Wallet, Settings } from 'lucide-react'
 import Link from 'next/link'
 
 const Sidebar = () => {
@@ -14,18 +10,18 @@ const Sidebar = () => {
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading='Suggestions'>
           <CommandItem>
-            <RxDashboard className='uppercase mr-2 h-4 w-4 text-main' />
+            <LayoutDashboard className='uppercase mr-2 h-4 w-4 text-main' />
             <Link href='/' className='uppercase'>
               Dashboard</Link>
           </CommandItem>
           <CommandItem>
-            <BsNewspaper className='uppercase mr-2 h-4 w-4 text-main' />
+            <Milestone className='uppercase mr-2 h-4 w-4 text-main' />
             <Link href='/posts' className='uppercase'>
               Posts
             </Link>
           </CommandItem>
           <CommandItem>
-            <PiFolders className='uppercase mr-2 h-4 w-4 text-main' />
+            <FolderClosed className='uppercase mr-2 h-4 w-4 text-main' />
             <Link href='#' className='uppercase'>
               Categories
             </Link>
@@ -34,17 +30,17 @@ const Sidebar = () => {
         <CommandSeparator />
         <CommandGroup heading='Settings'>
           <CommandItem>
-            <PiUserCircle className='text-main mr-2 h-4 w-4' />
+            <UserCircle className='text-main mr-2 h-4 w-4' />
             <span className='uppercase'>Account</span>
             <CommandShortcut>^A</CommandShortcut>
           </CommandItem>
           <CommandItem>
-            <ImCreditCard className='text-main mr-2 h-4 w-4' />
+            <Wallet className='text-main mr-2 h-4 w-4' />
             <span className='uppercase'>Payments</span>
             <CommandShortcut>^P</CommandShortcut>
           </CommandItem>
           <CommandItem>
-            <TbSettings className='text-main mr-2 h-4 w-4' />
+            <Settings className='text-main mr-2 h-4 w-4' />
             <span className='uppercase'>Settings</span>
             <CommandShortcut>^S</CommandShortcut>
           </CommandItem>
--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,6 +1,5 @@
-import { BsNewspaper } from 'react-icons/bs'
 import { Card, CardContent } from '../ui/card'
-import { LucideIcon } from 'lucide-react'
+import { LucideIcon, Milestone } from 'lucide-react'
 
 interface DashboardCardProps {
   title: string
@@ -14,7 +13,7 @@ const DashboardCard = ({ title, count, icon}: DashboardCardProps) => {
       <CardContent>
         <h3 className='text-3xl text-center mb-4 font-medium text-dark dark:text-light'>Posts</h3>
         <div className='flex gap-5 justify-center items-center'>
-          <BsNewspaper className='text-main' size={72} />
+          <Milestone className='text-main' size={72} />
           <h3 className='text-5xl font-normal text-dark dark:text-light'>43</h3>
         </div>
       </CardContent>
