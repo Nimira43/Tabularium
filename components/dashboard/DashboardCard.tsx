d5843c3a423b4a60ba7d518af6c355fee717b6c1
Nimira43 <n.rabone72@aol.com>
2025-06-11 16:27:55 +0100

    Passing in props to card.

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -11,10 +11,10 @@ const DashboardCard = ({ title, count, icon}: DashboardCardProps) => {
   return ( 
     <Card className='bg-grey-light-extra dark:bg-dark  p-4 pb-0'>
       <CardContent>
-        <h3 className='text-3xl text-center mb-4 font-medium text-dark dark:text-light'>Posts</h3>
+        <h3 className='text-3xl text-center mb-4 font-medium text-dark dark:text-light'>{title}</h3>
         <div className='flex gap-5 justify-center items-center'>
-          <Milestone className='text-main' size={72} />
-          <h3 className='text-5xl font-normal text-dark dark:text-light'>43</h3>
+          {icon}
+          <h3 className='text-5xl font-normal text-dark dark:text-light'>{count}</h3>
         </div>
       </CardContent>
 
