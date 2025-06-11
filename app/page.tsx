87ee0a0a98d1cf450999da4309681c6a8a4f3f16
Nimira43 <n.rabone72@aol.com>
2025-06-11 16:30:07 +0100

    Setting up prop in DashboardCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import DashboardCard from '@/components/dashboard/DashboardCard'
+import { Milestone } from 'lucide-react'
 
 export default function Home() {
   return (
     <>
       <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
-        <DashboardCard />
+        <DashboardCard 
+          title='Posts'
+          count={48}
+          icon={<Milestone className='text-main' size={72} />}
+        />
       </div>
     </>
   )
