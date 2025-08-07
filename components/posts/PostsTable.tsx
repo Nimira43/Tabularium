70dc8a01c79676e27c0f68187c285dad590a36e6
Nimira43 <n.rabone72@aol.com>
2025-08-07 21:26:52 +0100

    filteredPosts

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -14,7 +14,9 @@ const PostsTable = ({
 }: PostsTableProps) => {
   const sortedPosts: Post[] = [...posts].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
 
-
+  const filteredPosts = limit 
+    ? sortedPosts.slice(0, limit) 
+    : sortedPosts
 
   return ( 
     <div className='mt-10'>
