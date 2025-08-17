884fc4c0199a7ba99cdc9732c0c587e741c768d6
Nimira43 <n.rabone72@aol.com>
2025-08-17 10:24:10 +0100

    Brought in required imports for AnalyticsChart

--- components/dashboard/AnalyticsChart.tsx
+++ components/dashboard/AnalyticsChart.tsx
@@ -1,3 +1,7 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+import data from '@/data/analytics'
+
 import {} from 'recharts'
 
 const AnalyticsChart = () => {
