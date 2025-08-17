30ddc8d4b09fa2225dac05de8cc49d11d6521966
Nimira43 <n.rabone72@aol.com>
2025-08-17 09:53:45 +0100

    Created some made up data

--- data/analytics.ts
+++ data/analytics.ts
@@ -1,1 +1,78 @@
 import { AnalyticsItem } from '@/types/analytics'
+
+const data: AnalyticsItem[] = [
+  {
+    name: 'Jan',
+    uv: 3857,
+    pv: 2238,
+    amt: 2411,
+  },
+  {
+    name: 'Feb',
+    uv: 4289,
+    pv: 2848,
+    amt: 4284,
+  },
+  {
+    name: 'Mar',
+    uv: 1984,
+    pv: 3378,
+    amt: 2702,
+  },
+  {
+    name: 'Apr',
+    uv: 2339,
+    pv: 2086,
+    amt: 4397,
+  },
+  {
+    name: 'May',
+    uv: 3256,
+    pv: 2703,
+    amt: 5239,
+  },
+  {
+    name: 'Jun',
+    uv: 4298,
+    pv: 2583,
+    amt: 3937,
+  },
+  {
+    name: 'Jul',
+    uv: 4870,
+    pv: 4294,
+    amt: 5287,
+  },
+  {
+    name: 'Aug',
+    uv: 2643,
+    pv: 2290,
+    amt: 1858,
+  },
+  {
+    name: 'Sep',
+    uv: 2419,
+    pv: 2484,
+    amt: 1942,
+  },
+  {
+    name: 'Oct',
+    uv: 3510,
+    pv: 3263,
+    amt: 4576,
+  },
+  {
+    name: 'Nov',
+    uv: 5039,
+    pv: 4683,
+    amt: 4183,
+  },
+  {
+    name: 'Dec',
+    uv: 3857,
+    pv: 4188,
+    amt: 4538,
+  },
+]
+
+export default data
