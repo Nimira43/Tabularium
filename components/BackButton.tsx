776c265b729947a9cb97f3a3c6ce50c133b797d8
Nimira43 <n.rabone72@aol.com>
2025-08-30 00:53:09 +0100

    Passed BackButtonProps into BackButton. Imported Link

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,6 +1,15 @@
 import { BsArrowLeftCircle } from 'react-icons/bs'
+import Link from 'next/link'
 
 interface BackButtonProps {
   text: string
   link: string
 }
+
+const Backbutton = ({ text, link }: BackButtonProps) => {
+  return ( 
+
+  )
+}
+ 
+export default Backbutton
