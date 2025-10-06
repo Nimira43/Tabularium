fc289d6ed1cb7fda0d6a88148a8d015d4a772292
Nimira43 <n.rabone72@aol.com>
2025-10-06 22:02:18 +0100

    formSchema - 2

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -11,7 +11,18 @@ import { Button } from '@/components/ui/button'
 import posts from '@/data/posts'
 
 const formSchema = z.object({
-  
+  title: z.string().min(1, {
+    message: 'Title is required.'
+  }),
+  body: z.string().min(1, {
+    message: 'Body is required.'
+  }),
+  author: z.string().min(1, {
+    message: 'Author is required.'
+  }),
+  date: z.string().min(1, {
+    message: 'Date is required.'
+  }),
 })
 
 const PostEditPage = () => {
