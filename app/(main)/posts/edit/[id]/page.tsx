a9c0882a371d85c8be275fbd304b24756976397b
Nimira43 <n.rabone72@aol.com>
2025-10-06 21:55:47 +0100

    Import zodResolver and Form elements

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -3,6 +3,8 @@
 import Backbutton from '@/components/BackButton'
 import * as z from 'zod'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormItem, FormField, FormLabel, FormMessage } from '@/components/ui/form' 
 
 const PostEditPage = () => {
   return ( 
