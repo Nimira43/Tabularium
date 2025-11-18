e1ba2033b1b52903fb89f6f0eee8c0d9bc9d97f9
Nimira43 <n.rabone72@aol.com>
2025-11-18 02:26:58 +0000

    Wrapped ThemeProvider inside body tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,16 +24,18 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        <Navbar />
-        <div className='flex'>
-          <div className='hidden md:block h-[100vh] w-[300px]'>
-            <Sidebar />
+        <ThemeProvider>
+          <Navbar />
+          <div className='flex'>
+            <div className='hidden md:block h-[100vh] w-[300px]'>
+              <Sidebar />
+            </div>
+            <div className='p-5 w-full md:max-w-[1140px]'>
+              {children}
+            </div>
           </div>
-          <div className='p-5 w-full md:max-w-[1140px]'>
-            {children}
-          </div>
-        </div>
-        <Toaster />
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   )
