ca027aca8a7c198047653573a5c94b1a7a319023
Nimira43 <n.rabone72@aol.com>
2025-11-18 02:23:41 +0000

    ThemeProvider - 2

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -8,5 +8,10 @@ export function ThemeProvider({
   children, 
   ...props
 }: ThemeProviderProps) {
-  
-}
+  return (
+    <NextThemesProvider
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )}
