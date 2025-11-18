1357725fc1a942902b848f0b231e1a55c4d11146
Nimira43 <n.rabone72@aol.com>
2025-11-18 02:41:42 +0000

    ModeToggle - 2

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -7,4 +7,18 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          size='icon'
+        >
+          <HiOutlineSun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <HiOutlineMoon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100' />
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  )
 }
