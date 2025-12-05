470048278e846252f7ba96ec23c405be862d28b1
Nimira43 <n.rabone72@aol.com>
2025-12-05 21:01:53 +0000

    Placed Navbar and div containing Sidebar into MainLayout.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+
 const MainLayout = ({
   children
 }: { 
@@ -5,6 +8,15 @@ const MainLayout = ({
 }) => {
   return ( 
     <>
+      <Navbar />
+      <div className='flex'>
+        <div className='hidden md:block h-[100vh] w-[300px]'>
+          <Sidebar />
+        </div>
+        <div className='p-5 w-full md:max-w-[1140px]'>
+          {children}
+        </div>
+      </div>
     </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -37,12 +37,3 @@ export default function RootLayout({
     </html>
   )
 }
-<Navbar />
-          <div className='flex'>
-            <div className='hidden md:block h-[100vh] w-[300px]'>
-              <Sidebar />
-            </div>
-            <div className='p-5 w-full md:max-w-[1140px]'>
-              {children}
-            </div>
-          </div>
