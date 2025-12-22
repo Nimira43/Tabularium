4fdc8afae820c313d6b7693fdbef8ab56f621b68
Nimira43 <n.rabone72@aol.com>
2025-12-22 21:00:54 +0000

    Updated content.

--- components/auth/AuthTabs.tsx
+++ components/auth/AuthTabs.tsx
@@ -3,15 +3,15 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs'
 const AuthTabs = () => {
   return ( 
     <Tabs
-      defaultValue='account'
+      defaultValue='login'
       className='w-[400px]'
     >
       <TabsList className='grid w-full grid-cols-2'>
-        <TabsTrigger value='account'>Account</TabsTrigger>
-        <TabsTrigger value='password'>Password</TabsTrigger>
+        <TabsTrigger value='login'>Login</TabsTrigger>
+        <TabsTrigger value='register'>Register</TabsTrigger>
       </TabsList>
-      <TabsContent value='account'>Content 2333</TabsContent>
-      <TabsContent value='password'>Content 4242</TabsContent>
+      <TabsContent value='login'>Login</TabsContent>
+      <TabsContent value='register'>Register</TabsContent>
     </Tabs>
    )
 }
